bb56cec13e6ebbd32c0fd28bb482c4f0bd32de9b
abhishek <abhishekmrjbr@outlook.com>
2023-08-29 01:38:09 +0530

    i learn what is final and const and i use const here to make code easy like if i want to change something like color, height then i don't need to search full code i just look top where all const variable available and i change value from top

--- lib/input.dart
+++ lib/input.dart
@@ -1,5 +1,11 @@
 import 'package:flutter/material.dart';
 
+const bottomcontainerheight = 80.0;
+const topcontainercolor = Color(0xFF01D1F33);
+const center_container_color = Color(0xFF111428);
+const last_second_container_color = Color(0xFF111428);
+const bottom_container_color=Color(0xFFEB1555);
+
 class inputfile extends StatefulWidget {
   const inputfile({super.key});
 
@@ -9,8 +15,7 @@ class inputfile extends StatefulWidget {
 
 class _inputfileState extends State<inputfile> {
   Expanded struc({required Color C}) {
-    return Expanded(
-        child: container(C));
+    return Expanded(child: container(C));
   }
 
   Expanded rowstruc({required Color r}) {
@@ -32,22 +37,32 @@ class _inputfileState extends State<inputfile> {
         //backgroundColor: Color(0xFF0A0D22),
       ),
       body: Column(
-        children: [rowstruc(r: Colors.greenAccent), struc(C: Color(0xFF111428)), rowstruc(r: Colors.blue)],
+        children: [
+          rowstruc(r: topcontainercolor),
+          struc(C: center_container_color),
+          rowstruc(r: last_second_container_color),
+          Container(
+            color: bottom_container_color,
+            margin: EdgeInsets.only(top: 10),
+            width: double.infinity,
+            height: bottomcontainerheight,
+          )
+        ],
       ),
     );
   }
 }
 
 class container extends StatelessWidget {
-container(@required this.colour);
+  container(@required this.colour);
   final Color colour;
 
   @override
   Widget build(BuildContext context) {
     return Container(
       margin: EdgeInsets.all(15),
-      decoration: BoxDecoration(
-      borderRadius: BorderRadius.circular(10), color: colour),
+      decoration:
+          BoxDecoration(borderRadius: BorderRadius.circular(10), color: colour),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,12 @@
 import 'package:flutter/material.dart';
 import 'input.dart';
+
+const primary_color = Color(0xFF0A0D22);
+const appbar_theme_color = Color(0xFF0A0D22);
+const scaffold_background_color = Color(0xFF0A0D22);
+const text_theme_color = Color(0xFF81838D);
+
+
 void main() {
   runApp(bmi());
 }
@@ -13,11 +20,11 @@ class bmi extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(
-          primaryColor: Color(0xFF0A0D22),
-          appBarTheme: AppBarTheme(backgroundColor: Color(0xFF0A0D22)),
-          scaffoldBackgroundColor: Color(0xFF0A0D22),
+          primaryColor: primary_color ,
+          appBarTheme: AppBarTheme(backgroundColor: appbar_theme_color),
+          scaffoldBackgroundColor: scaffold_background_color,
           textTheme: const TextTheme(
-              bodyLarge: TextStyle(color: Color(0xFF81838D)))),
+              bodyLarge: TextStyle(color: text_theme_color))),
       //theme:ThemeData.dark(),
       home: inputfile() ,
     );
