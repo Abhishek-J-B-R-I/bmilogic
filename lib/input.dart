18712358004b5228a236cebda272ec4d1851e287
abhishek <abhishekmrjbr@outlook.com>
2023-09-14 11:44:52 +0530

    overlay

--- lib/input.dart
+++ lib/input.dart
@@ -4,10 +4,10 @@ import 'container.dart';
 import 'icono.dart';
 import 'contant_file.dart';
 
-enum genderr{
- male, female
-}
-int height_value_for_slider=186;
+enum genderr { male, female }
+
+int height_value_for_slider = 186;
+
 class inputfile extends StatefulWidget {
   const inputfile({super.key});
 
@@ -16,11 +16,9 @@ class inputfile extends StatefulWidget {
 }
 
 class _inputfileState extends State<inputfile> {
-
- genderr? selectgender;
+  genderr? selectgender;
   //Color maleinactivecolor= topcontainercolor;
   //Color femaleinactivecolor = topcontainercolor;
-
 
   //1=male , 2=female
 /*
@@ -54,22 +52,21 @@ void changeactivo(genderr gender){
   );
 }*/
 
-  Expanded struc({ required Color C, required Widget chi, genderr? gg}) {
-    return Expanded(child: container(C, chi, (){
-
+  Expanded struc({required Color C, required Widget chi, genderr? gg}) {
+    return Expanded(
+        child: container(C, chi, () {
       setState(() {
-        selectgender=gg!;
+        selectgender = gg!;
       });
-
     }));
   }
 
   Expanded rowstruc(
-      {  genderr? gm,genderr? gf, Color? r, Color? l,  Widget? ch,  Widget? ch2}) {
+      {genderr? gm, genderr? gf, Color? r, Color? l, Widget? ch, Widget? ch2}) {
     return Expanded(
         child: Row(
       children: [
-        struc(C: r!, chi: ch!,gg: gm),
+        struc(C: r!, chi: ch!, gg: gm),
         struc(C: l!, chi: ch2!, gg: gf),
       ],
     ));
@@ -112,40 +109,75 @@ void changeactivo(genderr gender){
       body: Column(
         children: [
           rowstruc(
-              r: selectgender==genderr.male?activecolor:topcontainercolor,l:selectgender==genderr.female?activecolor:topcontainercolor,gm: genderr.male,gf: genderr.female,
+              r: selectgender == genderr.male ? activecolor : topcontainercolor,
+              l: selectgender == genderr.female
+                  ? activecolor
+                  : topcontainercolor,
+              gm: genderr.male,
+              gf: genderr.female,
               ch: icono(iconoo: FontAwesomeIcons.mars, label: 'MALE'),
               ch2: icono(iconoo: FontAwesomeIcons.venus, label: 'Female')),
           struc(
-            C: center_container_color,
-            chi: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text('HEIGHT',style: label_text_style,),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.baseline,
-                  textBaseline: TextBaseline.alphabetic,
-                  children: [
-                    Text(height_value_for_slider.toString(),style: height_value,),
-                    Text('cm',style: label_text_style,)
-                  ],
-                ),
-                Slider(value: height_value_for_slider.toDouble(),max: 220.0,min: 120.0, onChanged: (double newValue) {
-                  setState(() {
-                   height_value_for_slider=newValue.round();
-                    print(newValue);
-                  });
-                },
-                  activeColor: height_slider_active_color,
-                  inactiveColor: height_slider_inactive_color,
+              C: center_container_color,
+              chi: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'HEIGHT',
+                    style: label_text_style,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.baseline,
+                    textBaseline: TextBaseline.alphabetic,
+                    children: [
+                      Text(
+                        height_value_for_slider.toString(),
+                        style: height_value,
+                      ),
+                      Text(
+                        'cm',
+                        style: label_text_style,
+                      )
+                    ],
+                  ),
+                  SliderTheme(
+                    data: SliderTheme.of(context).copyWith(
+                        thumbShape:
+                            RoundSliderThumbShape(enabledThumbRadius: 15),
+                        overlayShape:
+                            RoundSliderOverlayShape(overlayRadius: 30),
+activeTrackColor: Colors.white,
+                    inactiveTrackColor: Color(0xFF8E8F99),
 
-                )
-              ],
-            )
+                    thumbColor: Color(0xFFEB1555),
+                      overlayColor: Color(0x15EB1555)
 
-          ),
+                    ),
 
-          rowstruc(r: last_second_container_color,l: last_second_container_color, ch: Column(), ch2: Column()),
+                    child: Slider(
+                      value: height_value_for_slider.toDouble(),
+                      max: 220.0,
+                      min: 120.0,
+                      onChanged: (double newValue) {
+                        setState(() {
+                          height_value_for_slider = newValue.round();
+                          print(newValue);
+                        });
+                      },
+                      //activeColor: Colors.white,
+                     // inactiveColor: height_slider_inactive_color,
+                     // thumbColor: height_slider_active_color,
+                      //  overlayColor: Color(0xFFEB1555),
+                    ),
+                  )
+                ],
+              )),
+          rowstruc(
+              r: last_second_container_color,
+              l: last_second_container_color,
+              ch: Column(),
+              ch2: Column()),
           Container(
             color: bottom_container_color,
             margin: EdgeInsets.only(top: 10),
